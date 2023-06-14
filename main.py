4ed9b2e57c875c6c900090eedb6ebae1e48484c0
therolerofrome <105921270+therolerofrome@users.noreply.github.com>
2023-06-14 14:05:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+token = 'c170af50-1a63-46a0-8b5a-5da9156ceb52'
+url = 'http://external-integration.dev.t1support-portal.dev.ts/api/v1/tickets/'
+params = {
+    "theme": "string",
+    "numberFrom": 0,
+    "numberTo": 0,
+    "statuses": [
+      "NEW"
+    ],
+    "priorities": [
+      "NONE"
+    ],
+    "specialistIds": [
+      "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ],
+    "contractIds": [
+      "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ],
+    "systemIds": [
+      "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ],
+    "workGroupIds": [
+      "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ],
+    "tagIds": [
+      "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ],
+    "clientIds": [
+      "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ],
+    "tagName": "string",
+    "clientName": "string",
+    "specialistName": "string",
+    "systemName": "string",
+    "dateSolveFrom": "2022-11-03T09:06:28.932Z",
+    "dateSolveTo": "2022-11-03T09:06:28.932Z"
+}
+
+headers = {'Authorization': 'Bearer ' + token}
+
+response = requests.get(url,headers=headers, params=params)
+
+print(response.json())
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
